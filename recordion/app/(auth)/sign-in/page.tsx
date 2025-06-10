95acf2901aecec1c422ec1616b25db95558ee2e0
ElijDavis <elijahadavis@hotmail.com>
2025-06-10 18:13:36 -0400

    log in not working, go back and watch the section again. There were many intracate details that may have failed

--- recordion/app/(auth)/sign-in/page.tsx
+++ recordion/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { authClient } from "@/lib/auth-client"
 import Image from "next/image"
 import Link from 'next/link'
 
 const Page = () => {
+    const handleSignIn = async () => {
+        return await authClient.signIn.social({provider: 'google'})
+    }
     return(
         <main className="sign-in">
             <aside className="testimonial">
@@ -35,7 +41,7 @@ const Page = () => {
                             <h1>Recordion</h1>
                     </Link>
                     <p>Create and share your very first <span>Recordion video</span> in no time!</p>
-                    <button>
+                    <button onClick={handleSignIn}>
                         <Image src="/assets/icons/google.svg" alt="google" width={22} height={22} />
                         <span>Sign in with Google</span>
                     </button>
