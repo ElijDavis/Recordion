1fc4fb72a500947c24ed05e92f3876cd2eb6a511
ElijDavis <elijahadavis@hotmail.com>
2025-06-10 16:01:49 -0400

    Implemented the auth: doing the database now. If the addition of next to "devdependencies" doesn't work then come back to this commit

--- recordion/app/(root)/page.tsx
+++ recordion/app/(root)/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
 import Header from '@/components/Header'
+import VideoCard from '@/components/VideoCard'
+import { dummyCards } from '@/constants';
 
 export default function Home() {
   return (
     <main className="wrapper page">
       <Header title="All Videos" subHeader="Public Library"/>
+      <h1 className="text-2xl font-karla">Welcome to Recordion</h1>
+        <section className="video-grid">
+          {dummyCards.map((card) => (
+            <VideoCard key={card.id} {...card} />
+          ))}
+        </section>
     </main>
   );
 }
--- recordion/app/(root)/profile/[id]/page.tsx
+++ recordion/app/(root)/profile/[id]/page.tsx
@@ -1,11 +1,17 @@
 import Header from "@/components/Header";
+import VideoCard from '@/components/VideoCard'
+import { dummyCards } from '@/constants';
 
 const Page = async ({params}:ParamsWithSearch) => {
     const {id} = await params;
     return (
         <div className="wrapper page">
             <Header subHeader='elijahadavis@hotmail.com' title='Elijah Davis' userImg="/assets/images/dummy.jpg"/>
-            <h1 className="text-2xl font-karla">User ID: {id}</h1>
+                <section className="video-grid">
+                    {dummyCards.map((card) => (
+                        <VideoCard key={card.id} {...card} />
+                    ))}
+                </section>
         </div>
     )
 }
