be9e530500bc6b3246284329ab1fab91c553c8e8
ElijDavis <elijahadavis@hotmail.com>
2025-07-03 13:14:26 -0400

    I deleted everything from layout.tsx. I'm going to a completely new development so the recordion that is live will be deleted

--- recordion/app/(root)/layout.tsx
+++ recordion/app/(root)/layout.tsx
@@ -16,21 +16,6 @@ export default Layout*/
 
 import React, { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
-/*import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-import { NextRequest, NextResponse } from "next/server";
-
-    /*export async function middleware(request: NextRequest, response: NextResponse) {
-        const session = await auth.api.getSession({
-            headers: await headers()
-        })
-        if(!session) {
-            return NextResponse.redirect(new URL('/sign-in', request.url))
-        }
-    
-        return NextResponse.next();
-    }*/
 
 const Layout = async ({ children }: { children: ReactNode }) => {
 
